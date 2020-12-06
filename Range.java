ec9f65771568529785b97a00fdb225cea6d89d4f
Maxwell Zen <mzen20@stuy.edu>
2020-12-06 01:40:40 -0500

    drafted methods

---
+++ Range.java
@@ -0,0 +1,30 @@
+import java.util.NoSuchElementException;
+public class Range implements IntegerSequence{
+  private int start,end,current;
+  public Range(int start,  int end){
+    this.start = start;
+    this.end = end;
+    this.current = this.start;
+  }
+  public void reset(){
+    current = start;
+  }
+  public int length(){
+    return end - start + 1;
+  }
+  public boolean hasNext(){
+    return current <= end;
+  }
+
+  //@throws NoSuchElementException
+  public int next() throws NoSuchElementException{
+    if (hasNext()) {
+      int temp = current;
+      current += 1;
+      return temp;
+    } else {
+      throw new NoSuchElementException("There is no next element");
+    }
+  }
+
+}
