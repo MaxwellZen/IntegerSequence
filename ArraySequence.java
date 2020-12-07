d31a678bdccba5a1536956f52a2b6245354055e0
Maxwell Zen <mzen20@stuy.edu>
2020-12-07 12:55:32 -0500

    fixed the second ArraySequence constructor

--- ArraySequence.java
+++ ArraySequence.java
@@ -19,12 +19,14 @@ public class ArraySequence implements IntegerSequence{
     for (int i = 0; otherseq.hasNext(); i++) {
       ans[i]=otherseq.next();
     }
+    data = ans;
+    currentIndex = 0;
   }
   public void reset() {
     currentIndex=0;
   }
   public boolean hasNext() {
-    return currentIndex < data.length;
+    return this.currentIndex < this.data.length;
   }
   public int next() throws NoSuchElementException {
     if (hasNext()) {
